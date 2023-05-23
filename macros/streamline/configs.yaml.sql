a7cc6816e7acb8c7138319de00211013fe0cc31a
drethereum <trevor.wenokur@gmail.com>
2023-05-23 10:44:35 -0600

    new functions

--- macros/streamline/configs.yaml.sql
+++ macros/streamline/configs.yaml.sql
@@ -95,6 +95,28 @@
   sql: |
     {{ fsc_utils.sql_udf_json_rpc_call(False) }}
 
+- name: {{ schema }}.udf_simple_event_name
+  signature:
+    - [abi, VARIANT]
+  return_type: TEXT
+  options: |
+    LANGUAGE PYTHON
+    RUNTIME_VERSION = '3.8'
+    HANDLER = 'get_simplified_signature'
+  sql: |
+    {{ fsc_utils.create_udf_simple_event_names() }}
+
+- name: {{ schema }}.udf_keccak
+  signature:
+    - [event_name, VARCHAR(255)]
+  return_type: TEXT
+  options: |
+    LANGUAGE PYTHON
+    RUNTIME_VERSION = '3.8'
+    PACKAGES = ('pycryptodome==3.15.0')
+    HANDLER = 'udf_encode'
+  sql: |
+    {{ fsc_utils.create_udf_keccak() }}  
 
 {% endmacro %}
 
--- macros/streamline/functions.py.sql
+++ macros/streamline/functions.py.sql
@@ -40,3 +40,37 @@ def hex_to_int(encoding, hex) -> str:
   else:
     return str(int(hex, 16))
 {% endmacro %}
+
+{% macro create_udf_keccak() %}
+from Crypto.Hash import keccak
+
+def udf_encode(event_name):
+    keccak_hash = keccak.new(digest_bits=256)
+    keccak_hash.update(event_name.encode('utf-8'))
+    return '0x' + keccak_hash.hexdigest()
+{% endmacro %}
+
+{% macro create_udf_simple_event_names() %}
+
+def get_simplified_signature(abi):
+    def generate_signature(inputs):
+        signature_parts = []
+        for input_data in inputs:
+            if 'components' in input_data:
+                component_signature_parts = []
+                components = input_data['components']
+                component_signature_parts.extend(generate_signature(components))
+                component_signature_parts[-1] = component_signature_parts[-1].rstrip(",")
+                if input_data['type'].endswith('[]'):
+                    signature_parts.append("(" + "".join(component_signature_parts) + ")[],")
+                else:
+                    signature_parts.append("(" + "".join(component_signature_parts) + "),")
+            else:
+                signature_parts.append(input_data['type'].replace('enum ', '').replace(' payable', '') + ",")
+        return signature_parts
+
+    signature_parts = [abi['name'] + "("]
+    signature_parts.extend(generate_signature(abi['inputs']))
+    signature_parts[-1] = signature_parts[-1].rstrip(",") + ")"
+    return "".join(signature_parts)
+{% endmacro %}
