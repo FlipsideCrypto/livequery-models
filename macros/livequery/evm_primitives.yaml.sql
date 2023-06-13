c31d09774db28b6ff308c53eddc78fc5e236fd72
Julius Remigio <14811322+juls858@users.noreply.github.com>
2023-06-13 10:19:59 -0700

    - add error handling to evm primitives

--- macros/livequery/evm_primitives.yaml.sql
+++ macros/livequery/evm_primitives.yaml.sql
@@ -11,75 +11,75 @@
   signature:
     - [transaction, OBJECT]
     - [block_or_tag, STRING]
-  return_type: STRING
+  return_type: VARIANT
   options: |
     NOT NULL
     RETURNS NULL ON NULL INPUT
     VOLATILE
     COMMENT = $$Executes a new message call immediately without creating a transaction on the block chain.$$
   sql: |
-    {{ sql_live_rpc_call('eth_call', "[transaction, block_or_tag]", blockchain, "'mainnet'") | indent(4) -}}.result::STRING
+    {{ sql_live_rpc_call('eth_call', "[transaction, block_or_tag]", blockchain, "'mainnet'") | indent(4) -}}
 - name: {{ schema -}}.rpc_eth_call
   signature:
     - [transaction, STRING]
     - [block_or_tag, STRING]
     - [network, STRING]
-  return_type: STRING
+  return_type: VARIANT
   options: |
     NOT NULL
     RETURNS NULL ON NULL INPUT
     VOLATILE
     COMMENT = $$Executes a new message call immediately without creating a transaction on the block chain.$$
   sql: |
-    {{ sql_live_rpc_call('eth_call', '[transaction, block_or_tag]', blockchain, 'network') | indent(4) -}}.result::STRING
+    {{ sql_live_rpc_call('eth_call', '[transaction, block_or_tag]', blockchain, 'network') | indent(4) -}}
 
 - name: {{ schema -}}.rpc_eth_get_logs
   signature:
     - [filter, OBJECT]
-  return_type: ARRAY
+  return_type: VARIANT
   options: |
     NOT NULL
     RETURNS NULL ON NULL INPUT
     VOLATILE
     COMMENT = $$Returns an array of all logs matching filter with given address.$$
   sql: |
-    {{ sql_live_rpc_call('eth_getLogs', '[filter]', blockchain, "'mainnet'") | indent(4) -}}.result::ARRAY
+    {{ sql_live_rpc_call('eth_getLogs', '[filter]', blockchain, "'mainnet'") | indent(4) -}}
 - name: {{ schema -}}.rpc_eth_get_logs
   signature:
     - [filter, OBJECT]
     - [network, STRING]
-  return_type: ARRAY
+  return_type: VARIANT
   options: |
     NOT NULL
     RETURNS NULL ON NULL INPUT
     VOLATILE
     COMMENT = $$Returns an array of all logs matching filter with given address.$$
   sql: |
-    {{ sql_live_rpc_call('eth_getLogs', '[filter]', blockchain, 'network') | indent(4) -}}.result::ARRAY
+    {{ sql_live_rpc_call('eth_getLogs', '[filter]', blockchain, 'network') | indent(4) -}}
 
 - name: {{ schema -}}.rpc_eth_get_balance
   signature:
     - [address, STRING]
     - [block_or_tag, STRING]
-  return_type: STRING
+  return_type: VARIANT
   options: |
     NOT NULL
     RETURNS NULL ON NULL INPUT
     VOLATILE
     COMMENT = $$Returns the balance of the account of given address.$$
   sql: |
-    {{ sql_live_rpc_call('eth_getBalance', '[address, block_or_tag]', blockchain, "'mainnet'") | indent(4) -}}.result::STRING
+    {{ sql_live_rpc_call('eth_getBalance', '[address, block_or_tag]', blockchain, "'mainnet'") | indent(4) -}}
 - name: {{ schema -}}.rpc_eth_get_balance
   signature:
     - [address, STRING]
     - [block_or_tag, STRING]
     - [network, STRING]
-  return_type: STRING
+  return_type: VARIANT
   options: |
     NOT NULL
     RETURNS NULL ON NULL INPUT
     VOLATILE
     COMMENT = $$Returns the balance of the account of given address.$$
   sql: |
-    {{ sql_live_rpc_call('eth_getBalance', '[address, block_or_tag]', blockchain, 'network') | indent(4) -}}.result::STRING
+    {{ sql_live_rpc_call('eth_getBalance', '[address, block_or_tag]', blockchain, 'network') | indent(4) -}}
 {% endmacro %}
--- macros/livequery/utils.sql
+++ macros/livequery/utils.sql
@@ -2,10 +2,15 @@
 {#
     Helper macro to call a JSON RPC method on a live node.
  #}
+    WITH result as (
+        SELECT
+            live.udf_api(
+                '{endpoint}'
+                ,utils.udf_json_rpc_call('{{ method}}', {{ params }})
+                ,concat_ws('/', 'integration', _utils.udf_provider(), '{{ blockchain }}', {{ network }})
+            )::VARIANT:data AS data
+    )
     SELECT
-        live.udf_api(
-            '{endpoint}'
-            ,utils.udf_json_rpc_call('{{ method}}', {{ params }})
-            ,concat_ws('/', 'integration', _utils.udf_provider(), '{{ blockchain }}', {{ network }})
-        )::VARIANT:data
+        COALESCE(data:result, {'error':data:error})
+    FROM result
 {%- endmacro -%}
