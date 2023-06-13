5865d144749109233d30f2acf9cc652fc77a7611
Julius Remigio <14811322+juls858@users.noreply.github.com>
2023-06-13 13:03:06 -0700

    - add comments to signature and return types for evm rpc primitives

---
+++ analysis/test_crud_udfs_in_schema.sql
@@ -0,0 +1,1 @@
+{{- crud_udfs_in_schema(config_evm_rpc_primitives, "ethereum", None, drop_) -}}
--- macros/livequery/evm_primitives.yaml.sql
+++ macros/livequery/evm_primitives.yaml.sql
@@ -1,17 +1,17 @@
-{% macro config_evm_rpc_primitives(schema, blockchain) %}
-{#
+{%- macro config_evm_rpc_primitives(schema, blockchain) -%}
+{#-
     Generates a set of UDFs that call the Ethereum JSON RPC API
 
     - eth_call
     - eth_getLogs
     - eth_getBalance
 
- #}
+ -#}
 - name: {{ schema -}}.rpc_eth_call
   signature:
-    - [transaction, OBJECT]
-    - [block_or_tag, STRING]
-  return_type: VARIANT
+    - [transaction, OBJECT, The transaction object]
+    - [block_or_tag, STRING, The block number or tag to execute the call on]
+  return_type: [VARIANT, The return value of the executed contract code]
   options: |
     NOT NULL
     RETURNS NULL ON NULL INPUT
@@ -20,11 +20,11 @@
   sql: |
     {{ sql_live_rpc_call('eth_call', "[transaction, block_or_tag]", blockchain, "'mainnet'") | indent(4) -}}
 - name: {{ schema -}}.rpc_eth_call
-  signature:
-    - [transaction, STRING]
-    - [block_or_tag, STRING]
-    - [network, STRING]
-  return_type: VARIANT
+    signature:
+      - [transaction, OBJECT, The transaction object]
+      - [block_or_tag, STRING, The block number or tag to execute the call on]
+      - [network, STRING, The network to execute the call on]
+    return_type: [VARIANT, The return value of the executed contract code]
   options: |
     NOT NULL
     RETURNS NULL ON NULL INPUT
@@ -35,8 +35,8 @@
 
 - name: {{ schema -}}.rpc_eth_get_logs
   signature:
-    - [filter, OBJECT]
-  return_type: VARIANT
+    - [filter, OBJECT, The filter object]
+  return_type: [VARIANT, An array of all logs matching filter with given address]
   options: |
     NOT NULL
     RETURNS NULL ON NULL INPUT
@@ -46,9 +46,9 @@
     {{ sql_live_rpc_call('eth_getLogs', '[filter]', blockchain, "'mainnet'") | indent(4) -}}
 - name: {{ schema -}}.rpc_eth_get_logs
   signature:
-    - [filter, OBJECT]
-    - [network, STRING]
-  return_type: VARIANT
+    - [filter, OBJECT, The filter object]
+    - [network, STRING, The network to execute the call on]
+  return_type: [VARIANT, An array of all logs matching filter with given address]
   options: |
     NOT NULL
     RETURNS NULL ON NULL INPUT
@@ -59,9 +59,9 @@
 
 - name: {{ schema -}}.rpc_eth_get_balance
   signature:
-    - [address, STRING]
-    - [block_or_tag, STRING]
-  return_type: VARIANT
+    - [address, STRING, The address to get the balance of]
+    - [block_or_tag, STRING, The block number or tag to execute the call on]
+  return_type: [VARIANT, The balance of the account of given address]
   options: |
     NOT NULL
     RETURNS NULL ON NULL INPUT
@@ -71,10 +71,10 @@
     {{ sql_live_rpc_call('eth_getBalance', '[address, block_or_tag]', blockchain, "'mainnet'") | indent(4) -}}
 - name: {{ schema -}}.rpc_eth_get_balance
   signature:
-    - [address, STRING]
-    - [block_or_tag, STRING]
-    - [network, STRING]
-  return_type: VARIANT
+    - [address, STRING, The address to get the balance of]
+    - [block_or_tag, STRING, The block number or tag to execute the call on]
+    - [network, STRING, The network to execute the call on]
+  return_type: [VARIANT, The balance of the account of given address]
   options: |
     NOT NULL
     RETURNS NULL ON NULL INPUT
@@ -82,4 +82,4 @@
     COMMENT = $$Returns the balance of the account of given address.$$
   sql: |
     {{ sql_live_rpc_call('eth_getBalance', '[address, block_or_tag]', blockchain, 'network') | indent(4) -}}
-{% endmacro %}
+{% endmacro -%}
