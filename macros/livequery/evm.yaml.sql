64cf47e85ba54bdaa4f6f73ed43bb0a11a8aea11
Austin <austin@flipsidecrypto.com>
2023-06-14 17:22:16 -0400

    WIP

--- macros/livequery/evm.yaml.sql
+++ macros/livequery/evm.yaml.sql
@@ -3,4 +3,15 @@
     This macro is used to generate the high level abstractions for an EVM
     blockchain.
  #}
-{% endmacro -%}
+- name: {{ schema -}}.latest_native_balance
+  signature:
+    - [address, STRING, The address to get the balance of]
+  return_type: [TABLE (STRING, STRING, STRING, FLOAT)] 
+  options: |
+    NOT NULL
+    RETURNS NULL ON NULL INPUT
+    VOLATILE
+    COMMENT = $$Returns the native asset balance at the latest block for the address.$$
+  sql: |
+
+{%- endmacro -%}
