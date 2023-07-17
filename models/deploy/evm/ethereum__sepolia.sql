b2229932433c28bf35dfbcbc3bb161bb3c12b0b7
Julius Remigio <14811322+juls858@users.noreply.github.com>
2023-07-17 15:39:16 -0700

    - add depenency to live schema

--- models/deploy/evm/ethereum__sepolia.sql
+++ models/deploy/evm/ethereum__sepolia.sql
@@ -1,5 +1,6 @@
 -- depends_on: {{ ref('_internal__contracts_map') }}
 -- depends_on: {{ ref('_internal__abi_map') }}
+-- depends_on: {{ ref('live__') }}
 {%- set configs = [
     config_evm_rpc_primitives,
     config_evm_high_level_abstractions
