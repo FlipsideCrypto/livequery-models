eea05d567d6abad9b7a9fe503f8a91e7c324a229
Austin <austin@flipsidecrypto.com>
2023-07-19 19:56:58 -0400

    add deps

--- models/deploy/evm/arbitrum_nova__mainnet.sql
+++ models/deploy/evm/arbitrum_nova__mainnet.sql
@@ -2,6 +2,7 @@
 -- depends_on: {{ ref('_internal__abi_map') }}
 -- depends_on: {{ ref('_internal__native_symbol_map') }}
 -- depends_on: {{ ref('live__') }}
+-- depends_on: {{ ref('utils__') }}
 {%- set configs = [
     config_evm_rpc_primitives,
     config_evm_high_level_abstractions
