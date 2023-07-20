ed6d7ff0ff696fa89361f4f21521749808853903
Julius Remigio <14811322+juls858@users.noreply.github.com>
2023-07-20 12:34:55 -0700

    - fix tests - add tags

--- analysis/test_create_or_drop_function_from_config.sql
+++ analysis/test_create_or_drop_function_from_config.sql
@@ -1,4 +1,8 @@
-{%-  set udfs = fromyaml(config_core_udfs()) -%}
+{%-  set udfs = fromyaml(config_core_live()) -%}
+{% do udfs.extend(fromyaml(config_core__live())) %}
+{% do udfs.extend(fromyaml(config_core__utils())) %}
+{% do udfs.extend(fromyaml(config_core_utils())) %}
+
 {%- for udf in udfs -%}
 {{- create_or_drop_function_from_config(udf, drop_=True) -}}
 {{- create_or_drop_function_from_config(udf, drop_=False) -}}
