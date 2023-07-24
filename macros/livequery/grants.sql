a2007a81b4fd0e0ccad0ae3d4b31502533bc7cad
Julius Remigio <14811322+juls858@users.noreply.github.com>
2023-07-24 13:59:54 -0700

    - move grant_permissions_to_roles macro to `macros/livequery/grants.ql`

---
+++ macros/livequery/grants.sql
@@ -0,0 +1,21 @@
+{% macro grant_permissions_to_roles(schema) %}
+    {% if target.name == "prod" %}
+        {% set outer = namespace(sql="") %}
+        {% for role in ["VELOCITY_INTERNAL", "VELOCITY_ETHEREUM", "INTERNAL_DEV"] %}
+                {% set sql %}
+                    {% if schema.startswith("_") %}
+                        REVOKE USAGE ON SCHEMA {{ schema }} FROM {{ role }};
+                        REVOKE USAGE ON ALL FUNCTIONS IN SCHEMA {{ schema }} FROM {{ role }};
+                    {%- else -%}
+                        GRANT USAGE ON SCHEMA {{ schema }} TO {{ role }};
+                        GRANT USAGE ON ALL FUNCTIONS IN SCHEMA {{ schema }} TO {{ role }};
+
+                        GRANT SELECT ON ALL TABLES IN SCHEMA {{ schema }} TO {{ role }};
+                        GRANT SELECT ON ALL VIEWS IN SCHEMA {{ schema }} TO {{ role }};
+                    {%- endif -%}
+                {%- endset -%}
+                {% set outer.sql = outer.sql ~ sql %}
+        {%- endfor -%}
+        {{ outer.sql }}
+    {%- endif -%}
+{%- endmacro -%}
