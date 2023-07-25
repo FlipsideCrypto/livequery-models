16c034670ce32f7be45fe3824870c44ff88b2742
Jim Myers <jfmyers01@gmail.com>
2023-07-25 09:00:25 -0400

    Add helius api functions.

--- macros/marketplace/helius/util_udfs.yaml.sql
+++ macros/marketplace/helius/util_udfs.yaml.sql
@@ -3,6 +3,52 @@
     This macro is used to generate the Helius base endpoints
  #}
 
+- name: {{ schema -}}.get
+  signature:
+    - [NETWORK, STRING, The network 'devnet' or 'mainnet']
+    - [PATH, STRING, The API path starting with '/']
+    - [QUERY_PARAMS, OBJECT, The query parameters]
+  return_type:
+    - "VARIANT"
+  options: |
+    COMMENT = $$Used to issue an HTTP GET request to Helius.$$
+  sql: |
+    SELECT live.udf_api(
+      'GET',
+      CASE 
+          WHEN NETWORK = 'devnet' THEN 
+              concat('https://api-devnet.helius.xyz', PATH, '?api_key={API_KEY}&', utils.udf_object_to_url_query_string(QUERY_PARAMS))
+          ELSE 
+              concat('https://api.helius.xyz', PATH, '?api_key={API_KEY}&', utils.udf_object_to_url_query_string(QUERY_PARAMS))
+      END,
+      {},
+      {},
+      '_FSC_SYS/HELIUS'
+    ) as response
+
+- name: {{ schema -}}.post
+  signature:
+    - [NETWORK, STRING, The network 'devnet' or 'mainnet']
+    - [PATH, STRING, The API path starting with '/']
+    - [BODY, OBJECT, The request body]
+  return_type:
+    - "VARIANT"
+  options: |
+    COMMENT = $$Used to issue an HTTP POST request to Helius.$$
+  sql: |
+    SELECT live.udf_api(
+      'POST',
+      CASE
+          WHEN NETWORK = 'devnet' THEN 
+              concat('https://api-devnet.helius.xyz', PATH, '?api_key={API_KEY}')
+          ELSE 
+              concat('https://api.helius.xyz', PATH, '?api_key={API_KEY}')
+      END,
+      {},
+      BODY,
+      '_FSC_SYS/HELIUS'
+    ) as response
+
 - name: {{ schema -}}.rpc
   signature:
     - [NETWORK, STRING, The network 'devnet' or 'mainnet']
---
+++ models/deploy/marketplace/helius/helius_apis__helius_utils.sql
@@ -0,0 +1,6 @@
+-- depends_on: {{ ref('live') }}
+{%- set configs = [
+    config_helius_apis_udfs,
+    ] -%}
+{{- ephemeral_deploy_marketplace(configs) -}}
+-- depends_on: {{ ref('helius_utils__helius_utils') }}
