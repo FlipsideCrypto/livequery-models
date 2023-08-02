82a13d219b06d34bc62c7d25ff8a3b11142e0433
Julius Remigio <14811322+juls858@users.noreply.github.com>
2023-08-02 15:56:00 -0700

    - add test user id as project variable

--- tests/generic/test_udf.sql
+++ tests/generic/test_udf.sql
@@ -1,5 +1,10 @@
 
 {% test test_marketplace_udf(model, column_name, args, validations) %}
+    {#
+        This test is for UDFs that are defined in macros/marketplace.
+        The udfs are deployed using ephemeral models, so we need to
+        use the ephemeral model name to get the udf name.
+    #}
     {%- set schema = model | replace("__dbt__cte__", "") -%}
     {%- set schema = schema.split("__") | first -%}
     {%- set udf = schema ~ "." ~ column_name -%}
@@ -9,6 +14,7 @@
 
 {% test test_udf(model, column_name, args, validations) %}
     {%- set schema = model | replace("__dbt__cte__", "") -%}
+    {%- set schema = schema.split("__") | first -%}
     {%- set udf = schema ~ "." ~ column_name -%}
 
     {{ base_test_udf(model, udf, args, validations) }}
