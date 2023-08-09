d9e0dfb3c9693e34be3ac1057fc523d524d7de95
Jim Myers <jfmyers01@gmail.com>
2023-08-09 18:45:29 -0400

    New partner integrations (#36)

---
+++ macros/marketplace/cmc/udfs.yaml.sql
@@ -0,0 +1,42 @@
+{% macro config_cmc_udfs(schema_name = "cmc", utils_schema_name="cmc_utils") -%}
+{#
+    This macro is used to generate the CoinmarketCap Base endpoints
+ #}
+
+- name: {{ schema_name -}}.get
+  signature:
+    - [PATH, STRING, The path starting with '/']
+    - [QUERY_ARGS, OBJECT, The query arguments]
+  return_type:
+    - "VARIANT"
+  options: |
+    COMMENT = $$Used to issue a 'GET' request to the CoinmarketCap API. [CoinmarketCap docs here](https://coinmarketcap.com/api/documentation/v1/).$$
+  sql: |
+    SELECT
+      live.udf_api(
+        'GET',
+        concat('https://pro-api.coinmarketcap.com', PATH, '?', utils.udf_object_to_url_query_string(QUERY_ARGS)),
+        {'X-CMC_PRO_API_KEY': '{API_KEY}'},
+        {},
+        '_FSC_SYS/CMC'
+    ) as response
+
+- name: {{ schema_name -}}.post
+  signature:
+    - [PATH, STRING, The path starting with '/']
+    - [BODY, OBJECT, The request body]
+  return_type:
+    - "VARIANT"
+  options: |
+    COMMENT = $$Used to issue a 'POST' request to the CoinmarketCap API. [CoinmarketCap docs here](https://coinmarketcap.com/api/documentation/v1/).$$
+  sql: |
+    SELECT
+      live.udf_api(
+        'POST',
+        CONCAT('https://pro-api.coinmarketcap.com', PATH),
+        {'X-CMC_PRO_API_KEY': '{API_KEY}'},
+        BODY,
+        '_FSC_SYS/CMC'
+    ) as response
+
+{% endmacro %}
---
+++ macros/marketplace/defillama/udfs.yaml.sql
@@ -0,0 +1,40 @@
+{% macro config_defillama_udfs(schema_name = "defillama", utils_schema_name="defillama_utils") -%}
+{#
+    This macro is used to generate the Defillama endpoints
+ #}
+
+- name: {{ schema_name -}}.get
+  signature:
+    - [PATH, STRING, The path starting with '/']
+    - [QUERY_ARGS, OBJECT, The query arguments]
+  return_type:
+    - "VARIANT"
+  options: |
+    COMMENT = $$Used to issue a 'GET' request to the Defillama API. [Defillama docs here](https://defillama.com/docs/api).$$
+  sql: |
+    SELECT
+      live.udf_api(
+        'GET',
+        concat('https://api.llama.fi', PATH, '?', utils.udf_object_to_url_query_string(QUERY_ARGS)),
+        {},
+        {}
+    ) as response
+
+- name: {{ schema_name -}}.post
+  signature:
+    - [PATH, STRING, The path starting with '/']
+    - [BODY, OBJECT, The request body]
+  return_type:
+    - "VARIANT"
+  options: |
+    COMMENT = $$Used to issue a 'POST' request to the Defillama API. [Defillama docs here](https://defillama.com/docs/api).$$
+  sql: |
+    SELECT
+      live.udf_api(
+        'POST',
+        CONCAT('https://api.llama.fi', PATH),
+        {},
+        BODY
+    ) as response
+
+{% endmacro %}
