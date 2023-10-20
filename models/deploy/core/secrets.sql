31d5084cffbef61b29dfc78fbdbf340af1a91851
Julius Remigio <14811322+juls858@users.noreply.github.com>
2023-10-20 11:46:09 -0700

    Fix Secrets Schema Test compile bug (#59)

--- models/deploy/core/secrets.sql
+++ models/deploy/core/secrets.sql
@@ -1,6 +1,4 @@
 -- depends_on: {{ ref('_utils') }}
 -- depends_on: {{ ref('live') }}
-{% if var("ENABLE_SNOWFLAKE_SECRETS") %}
-  {% set config = config_core_secrets %}
-	{{ ephemeral_deploy_core(config) }}
-{% endif %}
+{% set config = config_core_secrets %}
+{{ ephemeral_deploy_core(config) }}
