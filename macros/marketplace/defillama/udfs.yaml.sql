64c644178a61a64e71ae3f9d36d575f0c6c11907
Julius Remigio <14811322+juls858@users.noreply.github.com>
2023-10-24 11:09:04 -0700

    LiveQuery DeFiLLama Api key integration, workflow updates for Warehouse (#61)

--- macros/marketplace/defillama/udfs.yaml.sql
+++ macros/marketplace/defillama/udfs.yaml.sql
@@ -17,24 +17,8 @@
         'GET',
         concat('https://api.llama.fi', PATH, '?', utils.udf_object_to_url_query_string(QUERY_ARGS)),
         {'Accept': '*/*', 'User-Agent': 'livequery/1.0 (Snowflake)', 'Host':'api.llama.fi', 'Connection': 'keep-alive'},
-        {}
-    ) as response
-
-- name: {{ schema_name -}}.post
-  signature:
-    - [PATH, STRING, The path starting with '/']
-    - [BODY, OBJECT, The request body]
-  return_type:
-    - "VARIANT"
-  options: |
-    COMMENT = $$Used to issue a 'POST' request to the Defillama API. [Defillama docs here](https://defillama.com/docs/api).$$
-  sql: |
-    SELECT
-      live.udf_api(
-        'POST',
-        CONCAT('https://api.llama.fi', PATH),
-        {'Accept': '*/*', 'User-Agent': 'livequery/1.0 (Snowflake)', 'Host':'api.llama.fi', 'Connection': 'keep-alive'},
-        BODY
+        {},
+        IFF(ARRAY_CONTAINS('api_key'::VARIANT, OBJECT_KEYS(QUERY_ARGS)), '_FSC_SYS/DEFILLAMA', '')
     ) as response
 
 {% endmacro %}
