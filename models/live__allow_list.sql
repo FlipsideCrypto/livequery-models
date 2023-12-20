793be8a78ae47ca908e4f238fab9f9b640f9cff2
Julius Remigio <14811322+juls858@users.noreply.github.com>
2023-12-20 09:08:13 -0800

    LiveQuery - Live view to show allowed apis (#79)

---
+++ models/live__allow_list.sql
@@ -0,0 +1,11 @@
+{{ config(
+    materialized = 'view',
+    grants = {'+select': fromyaml(var('ROLES'))}
+) }}
+
+SELECT '*.' || t.value AS allowed_domains
+FROM table(flatten(input => {{ this.database }}.live.udf_allow_list())) AS t
+ORDER BY
+    split_part(allowed_domains, '.', -1),
+    split_part(allowed_domains, '.', -2),
+    split_part(allowed_domains, '.', -3)
