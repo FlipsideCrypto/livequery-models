9f22dbcf75116148bdd045a445ac584a1c50f557
shah <info@shahnewazkhan.ca>
2024-02-16 15:19:54 -0800

    added create_udf_bulk_rest_api_v2 macro

---
+++ macros/streamline/udfs.sql
@@ -0,0 +1,16 @@
+{% macro create_udf_bulk_rest_api_v2() %}    
+    {% set sql %}
+    CREATE OR REPLACE EXTERNAL FUNCTION streamline.udf_bulk_rest_api_v2(json variant) returns variant api_integration = 
+    {% if target.name == "prod" %} 
+        {{ log("Creating prod udf_bulk_rest_api", info=True) }}
+        {{ var("API_INTEGRATION") }} AS https://{{ var("EXTERNAL_FUNCTION_URI") | lower }}/udf_bulk_rest_api
+    {% elif target.name == "dev" %}
+        {{ log("Creating dev udf_bulk_rest_api", info=True) }}
+        {{ var("API_INTEGRATION") }} AS https://{{ var("EXTERNAL_FUNCTION_URI") | lower }}/udf_bulk_rest_api
+    {% elif  target.name == "sbx" %}
+        {{ log("Creating stg udf_bulk_rest_api", info=True) }}
+        {{ var("API_INTEGRATION") }} AS https://{{ var("EXTERNAL_FUNCTION_URI") | lower }}/udf_bulk_rest_api
+    {%- endif %}
+    {% endset %}
+    {% do log(sql) %}
+{% endmacro %}
