a3b3fda53ee6f074a977db7957a738378c29d0f8
shah <info@shahnewazkhan.ca>
2024-04-14 14:15:05 -0700

    Add schema.udf specific options interpolation

--- macros/core/_live.yaml.sql
+++ macros/core/_live.yaml.sql
@@ -1,4 +1,6 @@
 {% macro config_core__live(schema="_live") %}
+{%- set api_integration_options = fromjson(var("API_INTEGRATION_OPTIONS")) -%}
+{%- set udf_api = schema ~ ".udf_api" -%}
 
 - name: {{ schema }}.udf_api
   signature:
@@ -11,7 +13,7 @@
   return_type: VARIANT
   func_type: EXTERNAL
   api_integration: '{{ var("API_INTEGRATION") }}'
-  api_integration_options : '{{ var("API_INTEGRATION_OPTIONS")[this.name]  if this.name in var("API_INTEGRATION_OPTIONS") else none }}'
+  api_integration_options : '{{ api_integration_options[udf_api] if udf_api in api_integration_options else none }}'
   options: |
     NOT NULL
     RETURNS NULL ON NULL INPUT
