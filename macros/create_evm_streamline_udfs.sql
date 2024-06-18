2b645b2935a627b542310ebdf0a358eb92894684
Austin <austin@flipsidecrypto.com>
2024-06-18 11:59:42 -0400

    empty for tags

--- macros/create_evm_streamline_udfs.sql
+++ macros/create_evm_streamline_udfs.sql
@@ -2,5 +2,5 @@
     {% if var("UPDATE_UDFS_AND_SPS") %}
         {{ create_udf_bulk_rest_api_v2_evm() }}
         {{ create_udf_bulk_decode_logs() }}
-    {% endif %}
+    {% endif %} 
 {% endmacro %}
