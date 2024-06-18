98adeb6e2a39b92cb9a43b8678f47c2d72deb837
Austin <austin@flipsidecrypto.com>
2024-06-18 11:36:50 -0400

    remove decoder create

--- macros/create_evm_streamline_udfs.sql
+++ macros/create_evm_streamline_udfs.sql
@@ -2,7 +2,6 @@
     {% if var("UPDATE_UDFS_AND_SPS") %}
         {% set sql %}
         {{ create_udf_bulk_rest_api_v2_evm() }}
-        {{ create_udf_bulk_decode_logs() }}
         {% endset %}
         {% do run_query(sql) %}
     {% endif %}
