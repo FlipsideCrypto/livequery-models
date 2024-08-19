8d0f440a2f064b9971637a779a5c3de1c3a1ca00
shah <info@shahnewazkhan.ca>
2024-08-19 16:50:48 -0700

    add integration_test tag

--- tests/generic/test_udf.sql
+++ tests/generic/test_udf.sql
@@ -1,9 +1,14 @@
-{% test test_udf(model, column_name, args, assertions) %}
+{% test test_udf(model, column_name, args, assertions, integration_test=false) %}
     {#
         This is a generic test for UDFs.
         The udfs are deployed using ephemeral models, so we need to
         use the ephemeral model name to get the udf name.
-     #}
+    #}
+
+    {% if integration_test %}
+        {{ config(tags=["integration_test"]) }}
+    {% endif %}
+
     {%- set schema = model | replace("__dbt__cte__", "") -%}
     {%- set schema = schema.split("__") | first -%}
     {%- set udf = schema ~ "." ~ column_name -%}
