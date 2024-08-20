aebc8a426af8c63bf1293c2e1afa3bb8e12db12d
shah <info@shahnewazkhan.ca>
2024-08-20 09:48:28 -0700

    add resouce path based tags

--- tests/generic/test_udf.sql
+++ tests/generic/test_udf.sql
@@ -4,11 +4,6 @@
         The udfs are deployed using ephemeral models, so we need to
         use the ephemeral model name to get the udf name.
     #}
-
-    {% if integration_test %}
-        {{ config(tags=["integration_test"]) }}
-    {% endif %}
-
     {%- set schema = model | replace("__dbt__cte__", "") -%}
     {%- set schema = schema.split("__") | first -%}
     {%- set udf = schema ~ "." ~ column_name -%}
