d786d4b85bae7880c060e302f1c2643e161576fa
drethereum <trevor.wenokur@gmail.com>
2024-08-28 15:53:18 -0600

    evm udf, decoded_traces and wait function

--- macros/streamline/udfs.sql
+++ macros/streamline/udfs.sql
@@ -46,6 +46,30 @@
     {% do adapter.execute(sql) %}
 {% endmacro %}
 
+{% macro create_udf_bulk_decode_traces() %}    
+    {{ log("Creating udf udf_bulk_decode_traces_v2 for target:" ~ target.name ~ ", schema: " ~ target.schema ~ ", DB: " ~ target.database, info=True) }}
+    {{ log("role:" ~ target.role ~ ", user:" ~ target.user, info=True) }}
+
+    {% set sql %}
+    CREATE OR REPLACE EXTERNAL FUNCTION streamline.udf_bulk_decode_traces_v2(json object) returns array api_integration = 
+    {% if target.name == "prod" %} 
+        {{ log("Creating prod udf_bulk_decode_traces_v2", info=True) }}
+        {{ var("API_INTEGRATION") }} AS 'https://{{ var("EXTERNAL_FUNCTION_URI") | lower }}bulk_decode_traces'
+    {% elif target.name == "dev" %}
+        {{ log("Creating dev udf_bulk_decode_traces_v2", info=True) }}
+        {{ var("API_INTEGRATION") }} AS 'https://{{ var("EXTERNAL_FUNCTION_URI") | lower }}bulk_decode_traces'
+    {% elif  target.name == "sbx" %}
+        {{ log("Creating stg udf_bulk_decode_traces_v2", info=True) }}
+        {{ var("API_INTEGRATION") }} AS 'https://{{ var("EXTERNAL_FUNCTION_URI") | lower }}bulk_decode_traces'
+    {% else %}
+        {{ log("Creating default (dev) udf_bulk_decode_traces_v2", info=True) }}
+        {{ var("config")["dev"]["API_INTEGRATION"] }} AS 'https://{{ var("config")["dev"]["EXTERNAL_FUNCTION_URI"] | lower }}bulk_decode_traces'
+    {% endif %};
+    {% endset %}
+    {{ log(sql, info=True) }}
+    {% do adapter.execute(sql) %}
+{% endmacro %}
+
 {% macro create_aws_api_integrations() %}    
     {{ log("Creating api integration for target:" ~ target.name ~ ", schema: " ~ target.schema ~ ", DB: " ~ target.database, info=True) }}
     {{ log("role:" ~ target.role ~ ", user:" ~ target.user, info=True) }}
