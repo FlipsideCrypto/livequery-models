eac2e05ee8c98acea3f7bd56a1b0e2da48e92b1d
Austin <austin@flipsidecrypto.com>
2024-09-25 16:33:53 -0400

    add

--- macros/create_streamline_udfs.sql
+++ macros/create_streamline_udfs.sql
@@ -1,11 +1,17 @@
 {% macro create_streamline_udfs() %}
     {% if var("UPDATE_UDFS_AND_SPS") %}
+        -- Create streamline schema if it doesn't exist
+        {% do run_query("CREATE SCHEMA IF NOT EXISTS streamline") %}
+        
         {{ create_udf_bulk_rest_api_v2() }}
     {% endif %}
 {% endmacro %}
 
 {% macro create_evm_streamline_udfs() %}
     {% if var("UPDATE_UDFS_AND_SPS") %}
+        -- Create streamline schema if it doesn't exist
+        {% do run_query("CREATE SCHEMA IF NOT EXISTS streamline") %}
+        
         {{ create_udf_bulk_rest_api_v2() }}
         {{ create_udf_bulk_decode_logs() }}
         {{ create_udf_bulk_decode_traces() }}
