eb94d0ffa9536fbda2fa1ccb4dd504b7d4aa8b11
Julius Remigio <14811322+juls858@users.noreply.github.com>
2024-11-06 11:04:06 -0800

    refactor evm_live_view_bronze_blocks macro to include blockchain and network parameters; add sql_live_rpc_batch_call helper macro for batch JSON RPC calls

--- macros/livequery/utils.sql
+++ macros/livequery/utils.sql
@@ -4,7 +4,7 @@
 
     Parameters:
         method (string): The JSON RPC method to call.
-        params (string): The JSON RPC parameters to pass to the method.
+        params (array): The JSON RPC parameters to pass to the method.
         blockchain (string): The blockchain to call the method on.
         network (string): The network to call the method on.
     Returns:
@@ -15,10 +15,34 @@
             live.udf_api(
                 '{endpoint}'
                 ,utils.udf_json_rpc_call({{ method }}, {{ params }})
-                ,concat_ws('/', 'integration', _utils.udf_provider(), {{ blockchain }}, {{ network }})
+                ,concat_ws('/', 'integration', _utils.udf_provider(), '{{ blockchain }}', '{{ network }}')
             )::VARIANT:data AS data
     )
     SELECT
         COALESCE(data:result, {'error':data:error})
     FROM result
+{% endmacro -%}
+
+{% macro sql_live_rpc_batch_call(from, calls, blockchain, network) %}
+{#
+    Helper macro to batch call a JSON RPC method on a live node.
+
+    Parameters:
+        calls (array): The JSON RPC parameters to pass to the method.
+        blockchain (string): The blockchain to call the method on.
+        network (string): The network to call the method on.
+    Returns:
+        string: The SQL to call the method.
+ #}
+    WITH result as (
+        SELECT
+            live.udf_api(
+                '{endpoint}'
+                ,{{ calls }}
+                ,concat_ws('/', 'integration', _utils.udf_provider(), {{ blockchain }}, {{ network }})
+            )::VARIANT:data::ARRAY AS data
+    )
+    SELECT
+        COALESCE(value:result, {'error':value:error})
+    FROM result, LATERAL FLATTEN(input => result.data) v
 {% endmacro -%}
