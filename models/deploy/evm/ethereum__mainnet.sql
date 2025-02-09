35e16e52fda9617636a70c82506d6486cc455c9e
Jensen Yap <jensen.yap@flipsidecrypto.com>
2025-02-10 00:31:02 +0800

    update changes from shah's branch

--- models/deploy/evm/ethereum__mainnet.sql
+++ models/deploy/evm/ethereum__mainnet.sql
@@ -5,6 +5,24 @@
 -- depends_on: {{ ref('_eth__decoded_logs') }}
 -- depends_on: {{ ref('live') }}
 -- depends_on: {{ ref('utils') }}
+-- depends_on: {{ ref('bronze__blocks') }}
+-- depends_on: {{ ref('bronze__blocks_fr') }}
+-- depends_on: {{ ref('bronze__transactions') }}
+-- depends_on: {{ ref('bronze__transactions_fr') }}
+-- depends_on: {{ ref('bronze__receipts') }}
+-- depends_on: {{ ref('bronze__receipts_fr') }}
+-- depends_on: {{ ref('bronze__traces') }}
+-- depends_on: {{ ref('bronze__traces_fr') }}
+-- depends_on: {{ ref('fsc_evm', 'silver__blocks') }}
+-- depends_on: {{ ref('fsc_evm', 'silver__transactions') }}
+-- depends_on: {{ ref('fsc_evm', 'silver__receipts') }}
+-- depends_on: {{ ref('fsc_evm', 'silver__traces') }}
+-- depends_on: {{ ref('evm__fact_blocks') }}
+-- depends_on: {{ ref('evm__fact_transactions') }}
+-- depends_on: {{ ref('fsc_evm', 'core__fact_blocks') }}
+-- depends_on: {{ ref('fsc_evm', 'core__fact_transactions') }}
+-- depends_on: {{ ref('fsc_evm', 'core__fact_event_logs') }}
+-- depends_on: {{ ref('fsc_evm', 'core__fact_traces') }}
 {%- set configs = [
     config_evm_rpc_primitives,
     config_evm_high_level_abstractions,
