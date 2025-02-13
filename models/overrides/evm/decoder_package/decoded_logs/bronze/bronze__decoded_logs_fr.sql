57ba812e4bf4b4b90af2c75ae6aab06b0c4fa430
Jensen Yap <jensen.yap@flipsidecrypto.com>
2025-02-13 10:59:40 +0800

    add stored procedure macro

--- models/overrides/evm/decoder_package/decoded_logs/bronze/bronze__decoded_logs_fr.sql
+++ models/overrides/evm/decoder_package/decoded_logs/bronze/bronze__decoded_logs_fr.sql
@@ -7,7 +7,7 @@
         tx_hash :: STRING || '-' || event_index :: STRING AS id,
         OBJECT_CONSTRUCT('topics', topics, 'data', data, 'address', contract_address) AS event_data,
         utils.udf_evm_decode_log(abi, event_data)[0] AS DATA,
-        TO_TIMESTAMP_NTZ(_inserted_timestamp) AS _inserted_timestamp
+        TO_TIMESTAMP_NTZ(inserted_timestamp) AS _inserted_timestamp
     FROM
         {{ ref('fsc_evm', 'core__fact_event_logs')}}
     JOIN
