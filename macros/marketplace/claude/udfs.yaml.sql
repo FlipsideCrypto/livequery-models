b7d472a3ae1a84f51fe13843bf70f66e048dcd31
Jensen Yap <jensen.yap@flipsidecrypto.com>
2025-02-17 17:29:46 +0900

    fix tests

--- macros/marketplace/claude/udfs.yaml.sql
+++ macros/marketplace/claude/udfs.yaml.sql
@@ -11,24 +11,14 @@
   options: |
     COMMENT = $$Make calls to Claude API [API docs: Claude](https://docs.anthropic.com/claude/reference/getting-started-with-the-api)$$
   sql: |
-    WITH base_headers AS (
-      SELECT CASE
-        WHEN PATH LIKE '/v1/messages%' THEN
-          {
-            'anthropic-version': '2023-06-01',
-            'x-api-key': '{API_KEY}',
-            'content-type': 'application/json'
-          }
-        ELSE
-          {
-            'content-type': 'application/json'
-          }
-      END as headers
-    )
     SELECT live.udf_api(
         'POST',
         CONCAT('https://api.anthropic.com', PATH),
-        (SELECT headers FROM base_headers),
+        {
+            'anthropic-version': '2023-06-01',
+            'x-api-key': '{API_KEY}',
+            'content-type': 'application/json'
+        },
         BODY,
         '_FSC_SYS/CLAUDE'
     ) as response
