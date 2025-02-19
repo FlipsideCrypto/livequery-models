6c367051d521c0d8936ec3ab9609d69c9b30c277
Jensen Yap <jensen.yap@flipsidecrypto.com>
2025-02-19 11:32:00 +0900

    add claude to marketplace

---
+++ macros/marketplace/claude/udfs.yaml.sql
@@ -0,0 +1,25 @@
+{% macro config_claude_udfs(schema_name = "claude", utils_schema_name = "claude_utils") -%}
+{#
+    This macro is used to generate API calls to Claude API endpoints
+ #}
+- name: {{ schema_name -}}.post
+  signature:
+    - [PATH, STRING, The API endpoint path]
+    - [BODY, OBJECT, The request body]
+  return_type:
+    - "VARIANT"
+  options: |
+    COMMENT = $$Make calls to Claude API [API docs: Claude](https://docs.anthropic.com/claude/reference/getting-started-with-the-api)$$
+  sql: |
+    SELECT live.udf_api(
+        'POST',
+        CONCAT('https://api.anthropic.com', PATH),
+        {
+            'anthropic-version': '2023-06-01',
+            'x-api-key': '{API_KEY}',
+            'content-type': 'application/json'
+        },
+        BODY,
+        '_FSC_SYS/CLAUDE'
+    ) as response
+{% endmacro %}
