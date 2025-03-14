f8f3e42e6be972ada11e6de44802dcf7885a0a34
shah <info@shahnewazkhan.ca>
2025-03-14 09:50:08 -0700

    add udf_test fix

--- tests/generic/test_udf.sql
+++ tests/generic/test_udf.sql
@@ -1,12 +1,42 @@
 {% test test_udf(model, column_name, args, assertions) %}
     {#
         This is a generic test for UDFs.
-        The udfs are deployed using ephemeral models, so we need to
-        use the ephemeral model name to get the udf name.
+        The udfs are deployed using ephemeral models, as of dbt-core > 1.8
+        we need to use `this.identifier` get schema.udf to test the udf.
      #}
-    {%- set schema = model | replace("__dbt__cte__", "") -%}
-    {%- set schema = schema.split("__") | first -%}
-    {%- set udf = schema ~ "." ~ column_name -%}
+    
+    {% set schema = none %}
+    
+    {# Try to extract schema from 'this' when executing #}
+    {% if execute %}
+        {# Extract schema based on standard pattern #}
+        {% set test_identifier = this.identifier %}
+        
+        {% if test_identifier.startswith('test_') %}
+            {% set test_identifier = test_identifier[5:] %}
+        {% endif %}
+        
+        {# Handle schemas with underscore prefix #}
+        {% if test_identifier.startswith('_') %}
+            {# For identifiers like _utils_<test_name> #}
+            {% set parts = test_identifier.split('_') %}
+            {% if parts | length > 2 %}
+                {# Reconstruct schema with underscore prefix #}
+                {% set schema = '_' ~ parts[1] %}
+            {% else %}
+                {# Fallback for simple cases #}
+                {% set schema = parts[0] %}
+            {% endif %}
+        {% else %}
+            {# For identifiers without underscore prefix #}
+            {% set parts = test_identifier.split('_') %}
+            {% if parts | length > 0 %}
+                {% set schema = parts[0] %}
+            {% endif %}
+        {% endif %}
+    {% endif %}
+    
+    {% set udf = schema ~ "." ~ column_name %}
 
     {{ base_test_udf(model, udf, args, assertions) }}
 {% endtest %}
