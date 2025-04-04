7573f25f951e3ab3cd8f5d8cc8cb931ff832130e
shah <info@shahnewazkhan.ca>
2025-04-04 16:32:35 -0700

    remove test tf definitions | add row_count to signature

--- macros/near/near.yaml.sql
+++ macros/near/near.yaml.sql
@@ -9,7 +9,7 @@
 - name: {{ schema -}}.tf_fact_blocks
   signature:
     - [_block_height, INTEGER, The start block height to get the blocks from]
-    - [to_latest, BOOLEAN, Whether to continue fetching blocks until the latest block or not]
+    - [row_count, INTEGER, The number of rows to fetch]
   return_type:
     - "TABLE(block_id NUMBER, block_timestamp TIMESTAMP_NTZ, block_hash STRING, block_author STRING, header OBJECT, block_challenges_result ARRAY, block_challenges_root STRING, chunk_headers_root STRING, chunk_tx_root STRING, chunk_mask ARRAY, chunk_receipts_root STRING, chunks ARRAY, chunks_included NUMBER, epoch_id STRING, epoch_sync_data_hash STRING, gas_price FLOAT, last_ds_final_block STRING, last_final_block STRING, latest_protocol_version INT, next_bp_hash STRING, next_epoch_id STRING, outcome_root STRING, prev_hash STRING, prev_height NUMBER, prev_state_root STRING, random_value STRING, rent_paid FLOAT, signature STRING, total_supply FLOAT, validator_proposals ARRAY, validator_reward FLOAT, fact_blocks_id STRING, inserted_timestamp TIMESTAMP_NTZ, modified_timestamp TIMESTAMP_NTZ)"
   options: |
@@ -23,7 +23,22 @@
 - name: {{ schema -}}.tf_fact_transactions
   signature:
     - [_block_height, INTEGER, The start block height to get the transactions from]
-    - [to_latest, BOOLEAN, Whether to continue fetching the next 100 blocks or not]
+    - [row_count, INTEGER, The number of rows to fetch]
+  return_type:
+    - "TABLE(tx_hash STRING, block_id NUMBER, block_timestamp TIMESTAMP_NTZ, nonce INT, signature STRING, tx_receiver STRING, tx_signer STRING, tx VARIANT, gas_used NUMBER, transaction_fee NUMBER, attached_gas NUMBER, tx_succeeded BOOLEAN, fact_transactions_id STRING, inserted_timestamp TIMESTAMP_NTZ, modified_timestamp TIMESTAMP_NTZ)"
+  options: |
+    NOT NULL
+    RETURNS NULL ON NULL INPUT
+    VOLATILE
+    COMMENT = $$Returns transaction details for blocks starting from a given height. Fetches up to the latest block if to_latest is true.$$
+  sql: |
+    {{ near_live_table_fact_transactions(schema, blockchain, network) | indent(4) -}}
+
+- name: {{ schema -}}.tf_fact_transactions_test
+  signature:
+    - [_block_height, INTEGER, The start block height to get the transactions from]
+    - [row_count, INTEGER, The number of rows to fetch]
+
   return_type:
     - "TABLE(tx_hash STRING, block_id NUMBER, block_timestamp TIMESTAMP_NTZ, nonce INT, signature STRING, tx_receiver STRING, tx_signer STRING, tx VARIANT, gas_used FLOAT, transaction_fee FLOAT, attached_gas FLOAT, tx_succeeded BOOLEAN, fact_transactions_id STRING, inserted_timestamp TIMESTAMP_NTZ, modified_timestamp TIMESTAMP_NTZ)"
   options: |
@@ -32,37 +47,7 @@
     VOLATILE
     COMMENT = $$Returns transaction details for blocks starting from a given height. Fetches up to the latest block if to_latest is true.$$
   sql: |
-    {{ near_live_table_fact_transactions_unabstracted(schema, blockchain, network) | indent(4) -}}
-
-- name: {{ schema -}}.tf_fact_transactions_test
-  signature:
-    - [_block_height, INTEGER, The start block height to get the transactions from]
-    - [to_latest, BOOLEAN, Whether to continue fetching blocks until we have the number of blocks specified in num_blocks or not]
-
-  return_type:
-    - "TABLE(tx_hash STRING, block_id NUMBER, block_timestamp TIMESTAMP_NTZ, nonce INT, signature STRING, tx_receiver STRING, tx_signer STRING, tx VARIANT, gas_used NUMBER, transaction_fee NUMBER, attached_gas NUMBER, tx_succeeded BOOLEAN, fact_transactions_id STRING, inserted_timestamp TIMESTAMP_NTZ, modified_timestamp TIMESTAMP_NTZ)"
-  options: |
-    NOT NULL
-    RETURNS NULL ON NULL INPUT
-    VOLATILE
-    COMMENT = $$Returns transaction details for blocks starting from a given height. Fetches up to the latest block if to_latest is true.$$
-  sql: |
-    {{ near_live_table_fact_transactions(schema, blockchain, network) | indent(4) -}}
-
-- name: {{ schema -}}.tf_fact_transactions_test
-  signature:
-    - [_block_height, INTEGER, The start block height to get the transactions from]
-    - [to_latest, BOOLEAN, Whether to continue fetching blocks until we have the number of blocks specified in num_blocks or not]
-    - [num_blocks, INTEGER, The number of block transactions to fetch (to_latest must be set to true)]
-  return_type:
-    - "TABLE(tx_hash STRING, block_id NUMBER, block_timestamp TIMESTAMP_NTZ, nonce INT, signature STRING, tx_receiver STRING, tx_signer STRING, tx VARIANT, gas_used NUMBER, transaction_fee NUMBER, attached_gas NUMBER, tx_succeeded BOOLEAN, fact_transactions_id STRING, inserted_timestamp TIMESTAMP_NTZ, modified_timestamp TIMESTAMP_NTZ)"
-  options: |
-    NOT NULL
-    RETURNS NULL ON NULL INPUT
-    VOLATILE
-    COMMENT = $$Returns transaction details for blocks starting from a given height. Fetches up to the latest block if to_latest is true.$$
-  sql: |
-    {{ near_live_table_fact_transactions(schema, blockchain, network) | indent(4) -}}
+    {{ near_live_table_fact_transactions_unabstracted(_bh_name='_block_height', _tl_name='to_latest') | indent(4) -}}
 
 {%- endmacro -%}
 
