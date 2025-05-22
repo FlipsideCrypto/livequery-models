c6525fb6ad775446f46a22e7e34ef5b9a6a9234e
shah <info@shahnewazkhan.ca>
2025-05-21 20:04:57 -0700

    update dev endpoint | add udf_api_batched to _live

--- macros/core/_live.yaml.sql
+++ macros/core/_live.yaml.sql
@@ -1,5 +1,21 @@
 {% macro config_core__live(schema="_live") %}
 
+- name: {{ schema }}.udf_api_batched
+  signature:
+    - [method, STRING]
+    - [url, STRING]
+    - [headers, OBJECT]
+    - [DATA, VARIANT]
+    - [user_id, STRING]
+    - [SECRET, STRING]
+  return_type: VARIANT
+  func_type: EXTERNAL
+  api_integration: '{{ var("API_INTEGRATION") }}'
+  max_batch_rows: '{{ var("MAX_BATCH_ROWS") }}'
+  options: |
+    NOT NULL
+  sql: udf_api
+
 - name: {{ schema }}.udf_api
   signature:
     - [method, STRING]
@@ -14,4 +30,5 @@
   options: |
     NOT NULL
   sql: udf_api
+
 {% endmacro %}
