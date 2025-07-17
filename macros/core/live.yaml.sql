fca84695cf2fccc3ea96d59f8651391c30e3eb83
Jensen Yap <jensen.yap@flipsidecrypto.com>
2025-07-17 20:23:20 +0900

    add udf_api_v2

--- macros/core/live.yaml.sql
+++ macros/core/live.yaml.sql
@@ -130,6 +130,95 @@
           secret_name
       )
 
+- name: {{ schema }}.udf_rpc
+  signature:
+    - [blockchain, STRING]
+    - [network, STRING]
+    - [method, STRING]
+    - [parameters, VARIANT]
+  return_type: VARIANT
+  options: |
+    VOLATILE
+    COMMENT = $$Executes an JSON RPC call on a blockchain.$$
+  sql: |
+    {{ sql_live_rpc_call("method", "parameters", "blockchain", "network") | indent(4) -}}
+
+- name: {{ schema }}.udf_allow_list
+  signature: []
+  return_type: ARRAY
+  func_type: EXTERNAL
+  api_integration: '{{ var("API_INTEGRATION") }}'
+  options: |
+    RETURNS NULL ON NULL INPUT
+    VOLATILE
+    COMMENT = $$Returns a list of allowed domains.$$
+  sql: allowed
+
+- name: {{ schema }}.udf_api_v2
+  signature:
+    - [url, STRING]
+    - [headers, OBJECT]
+    - [secret_name, STRING]
+  return_type: VARIANT
+  options: |
+    VOLATILE
+    COMMENT = $$Executes an LiveQuery Sync or Async Externall Function.$$
+  sql: |
+    SELECT
+        utils.udf_redirect_s3_presigned_url(
+            _live.udf_api_async('GET', url, headers, {}, _utils.UDF_WHOAMI(), secret_name)
+            :s3_presigned_url::STRING
+        ):data[0][1] as result
+    WHERE LOWER(COALESCE(
+        headers:"fsc-quantum-execution-mode"::STRING,
+        headers:"Fsc-Quantum-Execution-Mode"::STRING,
+        headers:"FSC-QUANTUM-EXECUTION-MODE"::STRING
+    )) = 'async'
+
+    UNION ALL
+
+    SELECT
+        _live.udf_api_sync('GET', url, headers, {}, _utils.UDF_WHOAMI(), secret_name) as result
+    WHERE LOWER(COALESCE(
+        headers:"fsc-quantum-execution-mode"::STRING,
+        headers:"Fsc-Quantum-Execution-Mode"::STRING,
+        headers:"FSC-QUANTUM-EXECUTION-MODE"::STRING,
+        'sync'
+    )) != 'async'
+
+- name: {{ schema }}.udf_api_v2
+  signature:
+    - [url, STRING]
+    - [headers, OBJECT]
+    - [secret_name, STRING]
+    - [is_async, BOOLEAN]
+  return_type: VARIANT
+  options: |
+    VOLATILE
+    COMMENT = $$Executes an LiveQuery Sync or Async Externall Function.$$
+  sql: |
+    SELECT
+        utils.udf_redirect_s3_presigned_url(
+            _live.udf_api_async('GET', url, headers, {}, _utils.UDF_WHOAMI(), secret_name)
+            :s3_presigned_url::STRING
+        ):data[0][1] as result
+    WHERE LOWER(COALESCE(
+        headers:"fsc-quantum-execution-mode"::STRING,
+        headers:"Fsc-Quantum-Execution-Mode"::STRING,
+        headers:"FSC-QUANTUM-EXECUTION-MODE"::STRING
+    )) = 'async'
+
+    UNION ALL
+
+    SELECT
+        _live.udf_api_sync('GET', url, headers, {}, _utils.UDF_WHOAMI(), secret_name) as result
+    WHERE LOWER(COALESCE(
+        headers:"fsc-quantum-execution-mode"::STRING,
+        headers:"Fsc-Quantum-Execution-Mode"::STRING,
+        headers:"FSC-QUANTUM-EXECUTION-MODE"::STRING,
+        'sync'
+    )) != 'async'
+
 - name: {{ schema }}.udf_api_v2
   signature:
     - [method, STRING]
@@ -160,7 +249,8 @@
     WHERE LOWER(COALESCE(
         headers:"fsc-quantum-execution-mode"::STRING,
         headers:"Fsc-Quantum-Execution-Mode"::STRING,
-        headers:"FSC-QUANTUM-EXECUTION-MODE"::STRING
+        headers:"FSC-QUANTUM-EXECUTION-MODE"::STRING,
+        'sync'
     )) != 'async'
 
 - name: {{ schema }}.udf_api_v2
@@ -191,27 +281,112 @@
             )
     END
 
-- name: {{ schema }}.udf_rpc
+- name: {{ schema }}.udf_api_v2
   signature:
-    - [blockchain, STRING]
-    - [network, STRING]
     - [method, STRING]
-    - [parameters, VARIANT]
+    - [url, STRING]
+    - [headers, OBJECT]
+    - [data, VARIANT]
   return_type: VARIANT
   options: |
     VOLATILE
-    COMMENT = $$Executes an JSON RPC call on a blockchain.$$
+    COMMENT = $$Executes an LiveQuery Sync or Async Externall Function.$$
   sql: |
-    {{ sql_live_rpc_call("method", "parameters", "blockchain", "network") | indent(4) -}}
+    SELECT
+        utils.udf_redirect_s3_presigned_url(
+            _live.udf_api_async(method, url, headers, data, _utils.UDF_WHOAMI(), '')
+            :s3_presigned_url::STRING
+        ):data[0][1] as result
+    WHERE LOWER(COALESCE(
+        headers:"fsc-quantum-execution-mode"::STRING,
+        headers:"Fsc-Quantum-Execution-Mode"::STRING,
+        headers:"FSC-QUANTUM-EXECUTION-MODE"::STRING
+    )) = 'async'
 
-- name: {{ schema }}.udf_allow_list
-  signature: []
-  return_type: ARRAY
-  func_type: EXTERNAL
-  api_integration: '{{ var("API_INTEGRATION") }}'
+    UNION ALL
+
+    SELECT
+        _live.udf_api_sync(method, url, headers, data, _utils.UDF_WHOAMI(), '') as result
+    WHERE LOWER(COALESCE(
+        headers:"fsc-quantum-execution-mode"::STRING,
+        headers:"Fsc-Quantum-Execution-Mode"::STRING,
+        headers:"FSC-QUANTUM-EXECUTION-MODE"::STRING,
+        'sync'
+    )) != 'async'
+
+- name: {{ schema }}.udf_api_v2
+  signature:
+    - [url, STRING]
+    - [data, VARIANT]
+  return_type: VARIANT
   options: |
-    RETURNS NULL ON NULL INPUT
     VOLATILE
-    COMMENT = $$Returns a list of allowed domains.$$
-  sql: allowed
+    COMMENT = $$Executes an LiveQuery Sync or Async Externall Function.$$
+  sql: |
+    SELECT
+        _live.udf_api_sync(
+          'POST',
+          url,
+          {'Content-Type': 'application/json'},
+          data,
+          _utils.UDF_WHOAMI(),
+          ''
+        )
+
+- name: {{ schema }}.udf_api_v2
+  signature:
+    - [url, STRING]
+    - [data, VARIANT]
+    - [secret_name, STRING]
+  return_type: VARIANT
+  options: |
+    VOLATILE
+    COMMENT = $$Executes an LiveQuery Sync or Async Externall Function.$$
+  sql: |
+    SELECT
+        _live.udf_api_sync(
+          'POST',
+          url,
+          {'Content-Type': 'application/json'},
+          data,
+          _utils.UDF_WHOAMI(),
+          secret_name
+        )
+
+- name: {{ schema }}.udf_api_v2
+  signature:
+    - [url, STRING]
+  return_type: VARIANT
+  options: |
+    VOLATILE
+    COMMENT = $$Executes an LiveQuery Sync or Async Externall Function.$$
+  sql: |
+    SELECT
+        _live.udf_api_sync(
+          'GET',
+          url,
+          {},
+          NULL,
+          _utils.UDF_WHOAMI(),
+          ''
+        )
+
+- name: {{ schema }}.udf_api_v2
+  signature:
+    - [url, STRING]
+    - [secret_name, STRING]
+  return_type: VARIANT
+  options: |
+    VOLATILE
+    COMMENT = $$Executes an LiveQuery Sync or Async Externall Function.$$
+  sql: |
+    SELECT
+        _live.udf_api_sync(
+          'GET',
+          url,
+          {},
+          {},
+          _utils.UDF_WHOAMI(),
+          secret_name
+        )
 {% endmacro %}
--- macros/livequery/external_access_integrations.sql
+++ macros/livequery/external_access_integrations.sql
@@ -4,7 +4,7 @@
   {% endset %}
 
   {% set network_rule_sql %}
-    CREATE OR REPLACE NETWORK RULE s3_express_network_rule
+    CREATE NETWORK RULE IF NOT EXISTS s3_express_network_rule
     MODE = EGRESS
     TYPE = HOST_PORT
     VALUE_LIST = (
@@ -15,17 +15,12 @@
   {% endset %}
 
   {% set external_access_sql %}
-    CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION s3_express_external_access_integration
+    CREATE EXTERNAL ACCESS INTEGRATION IF NOT EXISTS s3_express_external_access_integration
     ALLOWED_NETWORK_RULES = (s3_express_network_rule)
     ENABLED = true
   {% endset %}
 
   {% do run_query(use_schema_sql) %}
-  {% do log("Schema context set to live", true) %}
-
   {% do run_query(network_rule_sql) %}
-  {% do log("Network rule successfully created", true) %}
-
   {% do run_query(external_access_sql) %}
-  {% do log("External S3 Express access integration successfully created", true) %}
 {% endmacro %}
